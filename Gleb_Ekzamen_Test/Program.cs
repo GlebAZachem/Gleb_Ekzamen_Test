a371e55b18cd57fed813e1e55a19fe980d6b4bb1
NekoYo1 <kostyafear203@gmail.com>
2022-12-28 13:00:03 +1000

    Добавьте файлы проекта.

---
+++ Gleb_Ekzamen_Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gleb_Ekzamen_Test
+{
+    internal class Program
+    {
+        static int max(int x, int y)
+        {
+            if (x > y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите 1-е значение ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите 2-е значение ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите 3-е значение ");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите 4-е значение ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите 5-е значение ");
+            int e = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Значение максимального элемента = {0}", max(max(max(a, b), max(c, d)), e));
+            Console.ReadKey();
+        }
+
+    }
+}
